1f6c17b8f506e0814a7554a77f25fc7750fe6f8c
Mohan Prasath Chinnasamy <mohan.chinnasamy@cs.helsinki.fi>
2019-03-16 11:38:39 +0200

    Update

---
+++ game/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+"""
+A fun way to gamify mundane tasks.
+
+author: Mohan Prasath Chinnasamy
+"""
+
+import tkinter
+top = tkinter.Tk()
+# Code to add widgets will go here...
+top.mainloop()
