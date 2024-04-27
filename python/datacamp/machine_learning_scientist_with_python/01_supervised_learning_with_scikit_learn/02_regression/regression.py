26aae26e807cb1444ad25124bed54c6d252b08e2
Mohan Prasath Chinnasamy <mohanprasath@users.noreply.github.com>
2024-04-27 14:26:19 +0300

    MH Lesson#1 Chapter#3 Model Performance - begin

---
+++ python/datacamp/machine_learning_scientist_with_python/01_supervised_learning_with_scikit_learn/02_regression/regression.py
@@ -0,0 +1,162 @@
+import numpy as np
+import pandas as pd
+
+sales_df = pd.read_csv('advertising_and_sales_clean.csv')
+print(sales_df.head())
+print(sales_df.info())
+
+# Create X from the radio column's values
+X = sales_df['radio'].values
+
+# Create y from the sales column's values
+y = sales_df['sales'].values
+
+# Reshape X
+X = X.reshape(-1, 1)
+
+# Check the shape of the features and targets
+print(X.shape, y.shape)
+
+
+# Import LinearRegression
+from sklearn.linear_model import LinearRegression
+
+# Create the model
+reg = LinearRegression()
+
+# Fit the model to the data
+reg.fit(X, y)
+
+# Make predictions
+predictions = reg.predict(X)
+
+print(predictions[:5])
+
+
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+# Create scatter plot
+plt.scatter(X, y, color="blue")
+
+# Create line plot
+plt.plot(X, predictions, color="red")
+plt.xlabel("Radio Expenditure ($)")
+plt.ylabel("Sales ($)")
+
+# Display the plot
+plt.show()
+
+# y = ax+b
+'''
+y - tagret
+X - features
+
+a, b - model parameters
+
+loss functions - distance between the points and the red line (residuals)
+
+RSS = summation(yi - yibar) square error - squared
+
+in higher dimensions
+y = a1x1 + a2x2 + b
+
+
+'''
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Create X and y arrays
+X = sales_df.drop("sales", axis=1).values
+y = sales_df["sales"].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Instantiate the model
+reg = LinearRegression()
+
+# Fit the model to the data
+reg.fit(X_train, y_train)
+
+# Make predictions
+y_pred = reg.predict(X_test)
+print("Predictions: {}, Actual Values: {}".format(y_pred[:2], y_test[:2]))
+
+# Import mean_squared_error
+from sklearn.metrics import mean_squared_error
+
+# Compute R-squared
+r_squared = reg.score(X_test, y_test)
+
+# Compute RMSE
+rmse = mean_squared_error(y_test, y_pred, squared=False)
+
+# Print the metrics
+print("R^2: {}".format(r_squared))
+print("RMSE: {}".format(rmse))
+
+
+# Import the necessary modules
+from sklearn.model_selection import cross_val_score, KFold
+
+# Create a KFold object
+kf = KFold(n_splits=6, shuffle=True, random_state=5)
+
+reg = LinearRegression()
+
+# Compute 6-fold cross-validation scores
+cv_scores = cross_val_score(reg, X, y, cv=kf)
+
+# Print scores
+print(cv_scores)
+
+# Print the mean
+print(np.mean(cv_scores))
+
+# Print the standard deviation
+print(np.std(cv_scores))
+
+# Print the 95% confidence interval
+print(np.quantile(cv_scores, [0.025, 0.975]))
+
+# regularized regression - to avoid overfitting
+# ridge regression
+# hyper parameters - a vriable used to select model parameters
+# lasso - used to measure feature importance, features with non zero coefficients are selected for the model
+
+from sklearn.linear_model import Ridge, Lasso
+
+# Import Ridge
+from sklearn.linear_model import Ridge
+
+alphas = [0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0]
+ridge_scores = []
+for alpha in alphas:
+    # Create a Ridge regression model
+    ridge = Ridge(alpha=alpha)
+
+    # Fit the data
+    ridge.fit(X_train, y_train)
+
+    # Obtain R-squared
+    score = ridge.score(X_test, y_test)
+    ridge_scores.append(score)
+print(ridge_scores)
+
+
+# Import Lasso
+from sklearn.linear_model import Lasso
+
+# Instantiate a lasso regression model
+lasso = Lasso(alpha=0.3)
+
+# Fit the model to the data
+lasso.fit(X, y)
+
+# Compute and print the coefficients
+lasso_coef = lasso.coef_
+print(lasso_coef)
+plt.bar(sales_columns, lasso_coef)
+plt.xticks(rotation=45)
+plt.show()
