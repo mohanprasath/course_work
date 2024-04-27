c81ea2767aecf774d37cb18cc48e26e526827b46
Mohan Prasath Chinnasamy <mohanprasath@users.noreply.github.com>
2024-04-27 12:32:22 +0300

    MH Lesson#1 Chapter#1 train_test_split beings

--- python/datacamp/machine_learning_scientist_with_python/01_supervised_learning_with_scikit_learn/script.py
+++ python/datacamp/machine_learning_scientist_with_python/01_supervised_learning_with_scikit_learn/script.py
@@ -1,11 +1,8 @@
 # Import KNeighborsClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklear.model_selection import train_test_split
 
 y = churn_df["churn"].values
 X = churn_df[["account_length", "customer_service_calls"]].values
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=21, stratify=y)
 
 # Create a KNN classifier with 6 neighbors
 knn = KNeighborsClassifier(n_neighbors=6)
