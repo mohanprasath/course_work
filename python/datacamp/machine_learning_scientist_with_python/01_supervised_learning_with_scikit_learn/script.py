2ab18ae96d0a775929027992d3113f9294d7b4d0
Mohan Prasath Chinnasamy <mohanprasath@users.noreply.github.com>
2024-04-27 12:32:37 +0300

    MH Lesson#1 Chapter#1 train_test_split

--- python/datacamp/machine_learning_scientist_with_python/01_supervised_learning_with_scikit_learn/script.py
+++ python/datacamp/machine_learning_scientist_with_python/01_supervised_learning_with_scikit_learn/script.py
@@ -1,17 +1,15 @@
-# Import KNeighborsClassifier
-from sklearn.neighbors import KNeighborsClassifier
+# Import the module
+from sklearn.model_selection import train_test_split
 
+X = churn_df.drop("churn", axis=1).values
 y = churn_df["churn"].values
-X = churn_df[["account_length", "customer_service_calls"]].values
 
-# Create a KNN classifier with 6 neighbors
-knn = KNeighborsClassifier(n_neighbors=6)
+# Split into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42, stratify=y)
+knn = KNeighborsClassifier(n_neighbors=5)
 
-# Fit the classifier to the data
-knn.fit(X, y)
+# Fit the classifier to the training data
+knn.fit(X_train, y_train)
 
-# Predict the labels for the X_new
-y_pred = knn.predict(X_new)
-
-# Print the predictions
-print("Predictions: {}".format(y_pred))
+# Print the accuracy
+print(knn.score(X_test, y_test))
