80cf341ff1adaff806d87e7c3b4b8f6e945d67d7
Mohan Prasath Chinnasamy <mohanprasath@users.noreply.github.com>
2024-10-02 16:39:02 +0300

    Flask Ch#2 - Static & Dynamic URLs, Response and Methods

--- python/fun/flask/flask_tutorial_series/first_flask_project/README.md
+++ python/fun/flask/flask_tutorial_series/first_flask_project/README.md
@@ -8,3 +8,15 @@
 - Run the app.py using `python app.py`
 - A simple Hello world application
 - to save the current project pypi packages `pip freeze > requirements.txt`
+
+## Static and Dynamic URLS
+
+- For testing purposes, use `https://1127.0.0.1:5555/handle_url_params?greeting=Welcome&name=Satyaa`
+- Other options `curl https://1127.0.0.1:5555/handle_url_params?greeting=Welcome&name=Satyaa`
+- A sample POST in curl `curl -X POST https://1127.0.0.1:5555/handle_url_params?greeting=Welcome&name=Satyaa`
+- declaring type of mothods in code is `@app.route("handle_url_params", methods=['GET', 'POST', 'PUT', 'DELETE'])"`
+- Check the hello mwthods for example in ahndling multiple request types
+- Sepcific content type with status code is also feasible check `hello`
+- A sample curl request, ` curl -X POST -I http://127.0.0.1:5555/hello`
+- `make_response` can eb used to emulate the repsonse header and body programmatically in Flask
+- 
--- python/fun/flask/flask_tutorial_series/first_flask_project/firstapp/app.py
+++ python/fun/flask/flask_tutorial_series/first_flask_project/firstapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
 
@@ -6,9 +6,47 @@ app = Flask(__name__)
 def index():
     return "<h1>Welcome!</h1>"
 
-@app.route("/hello")
+@app.route("/hello", methods=['GET', 'POST', 'PUT', 'DELETE']) # static urls
 def hello():
-    return "<h1>Hello World!</h1>"
+    if request.method == 'GET':
+        return "<h1>Hello World! - From GET request</h1>", 200
+    elif request.method == 'POST':
+        return "<h1>Hello World! - From POST request</h1>", 200
+    elif request.method == 'PUT':
+        return "<h1>Hello World! - From PUT request</h1>", 200
+    return "Something is wrong", 400 # Bad Request Example
+
+@app.route('/hello_revamped', methods = ['GET', 'POST', 'PUT', 'DELETE'])
+def hello_revamped():
+    response = make_response('Hello Revamped!')
+    response.status_code = 202
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Content-Type'] = 'text/plain'
+    return response
+
+@app.route("/greet/<name>") # dynamic urls
+def greet(name):
+    return f"<h1>Hello, {name}!</h1>"
+
+@app.route("/add_old/<number1>/<number2>")#without type cast
+def add_without_typecast(number1, number2):
+    return f"{number1} + {number2} = {number1 + number2}"
+
+@app.route("/add/<int:number1>/<int:number2>") # with type cast
+def add(number1, number2):
+    return f"{number1} + {number2} = {number1 + number2}"
+
+
+@app.route('/handle_url_params')
+def handle_url_params():
+    greeting = request.args.get('greeting', '')
+    name = request.args.get('name', '')
+    if greeting and name:
+        return f'{greeting}, {name}'
+    else:
+        return 'Some params are missing!'
+    # return request.args.to_dict() # returns all arfuments as a dictionary
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5555, debug=True)
