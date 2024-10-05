e90b7a98845892e7c77501ac1d23d3d457e99047
Mohan Prasath Chinnasamy <mohanprasath@users.noreply.github.com>
2024-10-05 13:08:00 +0300

    Flask Update

---
+++ php/laracasts/01_php_for_beginners/demo/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>First PHP Document #1</title>
+</head>
+<body>
+    <h1> Hello World! </h1>
+    <h2> <?php
+    $greeting = "Hello";
+    echo $greeting . "World!";
+    ?> </h2>
+</body>
+</html>
